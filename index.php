7724ac20e089145f0ba8d30004f1f1b74cd534a7
flurbudurbur <sebastianhoogendijk@gmail.com>
2022-05-09 17:40:00 +0200

    push

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <meta name="description" content="">
     <title>Techlab Regius College</title>
 
+    <!-- scripts -->
+
     <!-- Stylesheets -->
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -21,15 +23,32 @@
         <div class="btn">
             <a class="<?php if ($page == 'home') {echo 'active';} ;?>" href="index.php">Home</a>
             <a class="" href="#">Reserveren</a>
-            <a class="" href="#">booking</a>
+            <a class="" href="#">Booking</a>
             <a class="" href="#">Login</a>
         </div>
     </header>
     <main>
-
+        <div class="slideshow">
+            <div class="slide">
+                <img src="assets/slideshow/slide1.jpg" alt="">
+                <p>stuff</p>
+            </div>
+            <div class="slide">
+                <img src="assets/slideshow/slide2.jpg" alt="">
+                <p>stuff</p>
+            </div>
+            <div class="slide">
+                <img src="assets/slideshow/slide3.jpg" alt="">
+                <p>stuff</p>
+            </div>
+            <div class="slide">
+                <img src="assets/slideshow/slide4.jpg" alt="">
+                <p>stuff</p>
+            </div>
+        </div>
     </main>
     <footer>
-        
+
     </footer>
     </body>
 
